a7325e501f187d787738ccb56c73dedee88bba06
merlox <merloxdixcontrol@gmail.com>
2017-06-10 11:55:44 +0200

    Changed shell

--- shell/shell.js
+++ shell/shell.js
@@ -1,10 +1,14 @@
+'use strict';
+
 const exec = require('child_process');
 const path = require('path');
 const fs = require('fs');
 
-const installData = `echo Configuring your autorresponder...
+const installData = `#!/bin/bash
+echo Configuring your autorresponder...
 sudo chmod +x autorresponder
 sudo cp autorresponder /usr/local/bin
+sudo chmod +x /usr/local/bin *
 
 cat << MERU >> ~/.bashrc
 export PATH=/usr/local/bin/autorresponder:$PATH
@@ -12,7 +16,14 @@ MERU
 
 source ~/.bashrc
 
-sudo bash -c 'cat << MERU >> /etc/systemd/system/autorresponder.service
+echo Stopping service in case it was enabled...
+sudo systemctl stop autorresponder.service
+echo Disabling service in case it was enabled...
+sudo systemctl disable autorresponder.service
+echo Removing files from past installations...
+sudo rm /lib/systemd/system/autorresponder.service
+
+sudo bash -c 'cat << MERU >> /lib/systemd/system/autorresponder.service
 [Unit]
 Description=A powerful and simple autorresponders server
 
@@ -29,15 +40,20 @@ Group=root
 WantedBy=multi-user.target
 MERU'
 
+echo Enabling service...
 sudo systemctl enable autorresponder.service
+echo Starting service...
 sudo systemctl start autorresponder.service
 echo Done, your autorresponder is now running`;
 
-const uninstallData = `echo Are you sure you want to uninstall the autorresponder? [y/N]
+const uninstallData = `#!/bin/bash
+echo Are you sure you want to uninstall the autorresponder? [y/N]
 read RESPONSE
 
 if [ $RESPONSE = 'y' ] || [ $RESPONSE = 'Y' ]; then
+   echo Response y
 else
+   echo Response not Y
    exit
 fi
 
@@ -50,10 +66,17 @@ sudo rm /usr/local/bin/autorresponder ~/.bashrc
 sudo cp /etc/skel/.bashrc ~/
 source ~/.bashrc
 
+echo Stopping service...
+sudo systemctl stop autorresponder.service
+echo Disabling service...
 sudo systemctl disable autorresponder.service
 
+echo Removing /etc/systemd/system/autorresponder.service...
 sudo rm /etc/systemd/system/autorresponder.service
-sudo rm $(ls -l /etc/systemd/system/multi-user.target.wants/ | cut -d ' ' -f 11 | grep autorresponder.service)
+echo Removing /lib/systemd/system/autorresponder.service...
+sudo rm /lib/systemd/system/autorresponder.service
+echo Removing /etc/systemd/system/multi-user.target.wants/autorresponder.service...
+sudo rm /etc/systemd/system/multi-user.target.wants/autorresponder.service
 
 echo Done, your autorresponder is now gone.`;
 
@@ -76,17 +99,21 @@ module.exports = {
             case 'install':
                fs.writeFile('install.sh', installData, err => {
                   if(err) console.log('Error writing the install file.');
+                  else console.log('Ok creating the install.sh file');
 
                   exec.exec('sudo chmod +x install.sh', (err, stdout, stderr) => {
                      if(err || stderr)
                         console.log('Error giving execution permission for the install file');
-                     exec.execFile('./install.sh', (err, stdout, stderr) => {
-                        if(err || stderr){
-                           console.log('');
-                           console.log('There was an error installing the app, try again with sudo');
-                           console.log('');
-                           console.log(stderr);
-                        }
+                     else
+                        console.log('Ok giving execution permission to the install.sh file');
+
+                     exec.exec('sudo ./install.sh', (err, stdout, stderr) => {
+                        console.log('ERR:');
+                        console.log(err);
+                        console.log('STDERR:');
+                        console.log(stderr);
+                        console.log('STDOUT:');
+                        console.log(stdout);
 
                         cb(null);
                      });
@@ -99,15 +126,16 @@ module.exports = {
                   if(err) console.log('Error writing the uninstall file.');
 
                   exec.exec('sudo chmod +x uninstall.sh', (err, stdout, stderr) => {
-                     if(err || stderr)
-                        console.log('Error giving execution permission for the uninstall file');
-                     exec.execFile('./uninstall.sh', (err, stdout, stderr) => {
-                        if(err || stderr){
-                           console.log('');
-                           console.log('There was an error uninstalling the app, try again with sudo');
-                           console.log('');
-                           console.log(stderr);
-                        }
+                     if(err || stderr) console.log('Error giving execution permission for the install file');
+                     else console.log('Ok giving execution permission to the install.sh file');
+
+                     exec.exec('sudo ./uninstall.sh', (err, stdout, stderr) => {
+                        console.log('ERR:');
+                        console.log(err);
+                        console.log('STDERR:');
+                        console.log(stderr);
+                        console.log('STDOUT:');
+                        console.log(stdout);
 
                         cb(null);
                      });
